51b648dd15308fbab891b5feef2a0400992af99d
Mishchenko  Sergey <sergey994049@gmail.com>
2025-12-07 13:26:03 +0900

    init

--- include/mini_file_editor_lib.h
+++ include/mini_file_editor_lib.h
@@ -2,14 +2,12 @@
 #define MINI_FILE_EDITOR_LIB_H
 #define MAX_LINE_LENGTH 1024
 
-// Структура для представления строки текста
 typedef struct Line {
     char text[MAX_LINE_LENGTH];
     struct Line* next;
     struct Line* prev;
 } Line;
 
-// Структура для представления редактора
 typedef struct {
     Line* head;
     Line* tail;
@@ -17,7 +15,6 @@ typedef struct {
     int line_count;
 } TextEditor;
 
-// Прототипы функций
 TextEditor* create_editor();
 void free_editor(TextEditor* editor);
 int insert_line(TextEditor* editor, const char* text, int position);
